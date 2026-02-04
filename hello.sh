fde5896d09b64d46e85530b6392922420f2ad4fe
shashi9133 <shashidharnani22@gmail.com>
2026-02-04 17:59:22 +0530

    updated

--- hello.sh
+++ hello.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-
-echo "Hello from shell script"
-echo "User: $USER"
-echo "Current Directory: $(pwd)"
+USERID=$(id-u)
+if[$USERID -ne 0]
+then 
+echo "Error:: please run this this script with root access"
+fi
+yum install postfix -y
+if[$? -ne 0]
+then 
+echo "Installation of postfix is error"
+exit 1
+else
+echo "installation of postfix is success"
+fi
+yum install maven -y
+if[$? -ne 0]
+then 
+echo "installation of maven is error"
+exit 1
+else
+echo "installation of maveen is success"
+fi
